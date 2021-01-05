9637d1f597ee0d821c9876350f3dbfa1cd1e9dc7
Fons van der Plas <fonsvdplas@gmail.com>
2021-01-05 23:48:19 +0100

    no more file

--- render_notebooks.jl
+++ render_notebooks.jl
@@ -1,8 +1,3 @@
-output_dir = "__notebook_exports"
-output_dir = "."
-
-isdir(output_dir) || mkdir(output_dir)
-
 using Pkg;
 env = mktempdir();
 Pkg.activate(env);
@@ -11,20 +6,6 @@ Pkg.add([
     Pkg.PackageSpec(url="https://github.com/fonsp/Pluto.jl", rev="b4f583f"),
 ]);
 
-import PlutoUtils.Export;
+import PlutoUtils;
 
-
-jlfiles = vcat(map(walkdir(".")) do (root, dirs, files)
-    map(
-        filter(files) do file
-            occursin(".jl", file)
-        end
-        ) do file
-        joinpath(root, file)
-    end
-end...);
-notebookfiles = filter(jlfiles) do f
-    readline(f) == "### A Pluto.jl notebook ###"
-end;
-
-Export.export_paths(notebookfiles; export_dir=output_dir, copy_to_temp_before_running=false);
+PlutoUtils.Export.github_action(; export_dir=".");
